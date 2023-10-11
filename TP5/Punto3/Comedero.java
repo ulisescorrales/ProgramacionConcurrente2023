29ef766bc3bb9b57ba86ff49fd75771301277ae4
Ulises-FAI <ulises.corrales@est.fi.uncoma.edu.ar>
2023-10-11 12:28:11 -0300

    realizando punto de perros y gatos

--- TP5/Punto3/Comedero.java
+++ TP5/Punto3/Comedero.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package programacionconcurrente2023.TP5.Punto2;
+package programacionconcurrente2023.TP5.Punto3;
 
 import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
--- TP5/Punto3/Gato.java
+++ TP5/Punto3/Gato.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package programacionconcurrente2023.TP5.Punto2;
+package programacionconcurrente2023.TP5.Punto3;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,20 +11,20 @@ import java.util.logging.Logger;
  *
  * @author ulisescorrales
  */
-public class Perro extends Thread{
+public class Gato extends Thread{
     Comedero comedero;
 
-    public Perro(Comedero comedero, String name) {
+    public Gato(Comedero comedero, String name) {
         super(name);
         this.comedero = comedero;
     }
     
     public void run(){
-        comedero.comerPerro();
+        comedero.comerGato();
         try {
             Thread.sleep(2000);
         } catch (InterruptedException ex) {
-            Logger.getLogger(Perro.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Gato.class.getName()).log(Level.SEVERE, null, ex);
         }
         comedero.dejarComer();
     }
